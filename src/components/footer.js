bd37bef468dbed6d6262cf14e1110b4bd1f82de2
iamrishabhnirmalkar <iamrishabhnirmalkar@gmail.com>
2023-07-12 16:39:27 +0530

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,16 @@ import React from "react";
 import Logo from "../assets/logo.svg";
 
 function footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <div className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023 All Rights Reserved. <br className="my-2" /> Designed And
-            maintained by Rishabh Nirmalkar
+            © {currentYear} All Rights Reserved. <br className="my-2" />{" "}
+            Designed And maintained by Rishabh Nirmalkar
           </span>
           <div className="flex justify-center items-center my-4">
             <a href="/" className="flex items-center">
